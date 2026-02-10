4247f419d6b3f64819b81a00d18ef0b240fc7591
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-02-10 14:08:47 +0600

    node added component added inside the base node

--- components/home/node/BaseNode.tsx
+++ components/home/node/BaseNode.tsx
@@ -1,4 +1,7 @@
-import { Handle, Position } from "@xyflow/react";
+import { Handle, Position, useNodeId } from "@xyflow/react";
+import clsx from "clsx";
+import { useState } from "react";
+import NodeAdder from "../NodeAdder";
 
 type BaseNodeProps = {
   children: React.ReactNode;
@@ -13,34 +16,40 @@ export function BaseNode({
   hideDefaultHandles = false,
   className = "",
 }: BaseNodeProps) {
+  const [open, setOpen] = useState<boolean>(false);
+  const nodeId = useNodeId();
   return (
-    <div
-      className={[
-        "rounded-xl border bg-white px-4 py-3 shadow-sm min-w-55 transition-all duration-150",
-        selected
-          ? "border-blue-400 shadow-md ring-2 ring-blue-100"
-          : "border-gray-200 hover:border-gray-300 hover:shadow-md",
-        className,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    >
-      {!hideDefaultHandles && (
-        <>
-          <Handle
-            type="target"
-            position={Position.Top}
-            className="w-3! h-3! bg-white! border-2! border-gray-300! hover:border-blue-400! transition-colors"
-          />
-          <Handle
-            type="source"
-            position={Position.Bottom}
-            className="w-3! h-3! bg-white! border-2! border-gray-300! hover:border-blue-400! transition-colors"
-          />
-        </>
-      )}
+    <>
+      <div
+        className={clsx(
+          "rounded-xl border bg-white px-4 py-3 shadow-sm min-w-55 transition-all duration-150",
+          {
+            "border-blue-400 shadow-md ring-2 ring-blue-100": selected,
+            "border-gray-200 hover:border-gray-300 hover:shadow-md": !selected,
+            className,
+          },
+        )}
+      >
+        {!hideDefaultHandles && (
+          <>
+            <Handle
+              type="target"
+              position={Position.Top}
+              className="w-3! h-3! bg-white! border-2! border-gray-300! hover:border-blue-400! transition-colors"
+            />
+            <Handle
+              type="source"
+              position={Position.Bottom}
+              className="w-3! h-3! bg-white! border-2! border-gray-300! hover:border-blue-400! transition-colors"
+            />
+          </>
+        )}
 
-      {children}
-    </div>
+        {children}
+      </div>
+      {selected && (
+        <NodeAdder selectedNodeId={nodeId} open={open} setOpen={setOpen} />
+      )}
+    </>
   );
 }
